669e1466932f6d036000e4c0562bb8a708eb25c2
TusharShandilya <tusharshandilya@live.com>
2021-07-06 09:24:16 +0530

    Board card component added

---
+++ src/components/UI/molecules/BoardCard/BoardCard.jsx
@@ -0,0 +1,17 @@
+import { Heading } from "../../atoms";
+
+import styles from "./boardCard.module.scss";
+
+const BoardCard = ({ title, slug, background }) => {
+  return (
+    <div className={styles.card}>
+      <Heading size="4">{title}</Heading>
+      <div className={styles.actions}>
+        <button>delete</button>
+        <button>go to board</button>
+      </div>
+    </div>
+  );
+};
+
+export default BoardCard;
