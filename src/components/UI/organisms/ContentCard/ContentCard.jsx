5a597134824c9417ccac5f33d7ece0ff0cd64a22
TusharShandilya <tusharshandilya@live.com>
2021-07-21 19:52:13 +0530

    Draggable intitial research

--- src/components/UI/organisms/ContentCard/ContentCard.jsx
+++ src/components/UI/organisms/ContentCard/ContentCard.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
 
-import { Button, Card } from "../../atoms";
+import { Card } from "../../atoms";
 
 import styles from "./contentCard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
 
-const ContentCard = ({ card }) => {
+const ContentCard = ({ card, ...rest }) => {
   const { id, content } = card;
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+  // const contentCardStyles = useMemo(
+  //   () =>
+  //     classNames(styles.card, {
+  //       [styles.isDraggedOver]: isDraggedOver,
+  //     }),
+  //   [isDraggedOver]
+  // );
+  const contentCardStyles = `${styles.card} ${
+    isDraggedOver ? styles.isDraggedOver : ""
+  }`;
+
+  const handleOnDragEnter = (event) => {
+    event.preventDefault();
+    console.log("enter");
+    toggleDragClass(true);
+
+    // Delete the card from the list and add it to it's new position
+  };
+
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+    console.log("leave");
+    toggleDragClass(false);
+  };
+
+  const toggleDragClass = (val) => {
+    console.table({ toggled: val });
+    setIsDraggedOver(val);
+  };
 
   return (
-    <Card className={styles.card}>
+    <Card
+      onDragEnter={handleOnDragEnter}
+      // onDragLeave={handleDragLeave}
+      id={id}
+      {...rest}
+      className={contentCardStyles}
+    >
+      {/* TODO: add an overlay 200% the size */}
       <div className={styles.content}>{content}</div>
       <div className={styles.menu}>
         <FontAwesomeIcon icon={faPenSquare} />
--- src/components/UI/organisms/ListCard/ListCard.jsx
+++ src/components/UI/organisms/ListCard/ListCard.jsx
@@ -1,19 +1,61 @@
+/**
+ * NOTES:
+ * TODO: if card is being added to the same list
+ * on darg over add a class to add empty space
+ */
+
 import styles from "./listCard.module.scss";
 
 import { Card, Heading } from "../../atoms";
 import ContentCard from "../ContentCard/ContentCard";
+import { useState } from "react";
+
+const dragUtils = {};
 
 const ListCard = ({ title, cards }) => {
+  const [cardsLocal, setCardsLocal] = useState(cards);
+
+  const handleOnDragStart = (cardInfo) => (event) => {
+    event.dataTransfer.setData("text", JSON.stringify(cardInfo));
+    console.log(event);
+  };
+
+  const handleOnDrop = (event) => {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+
+    // DO this using redux:
+    // setCardsLocal((p) => [...p, JSON.parse(data)]);
+  };
+
+  const handleCardDragEnd = (position, id) => (event) => {
+    event.preventDefault();
+    console.log(position, id);
+    // Delete the card from the list and add it to it's new position
+  };
+
   return (
-    <Card className={styles.card}>
+    <Card
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={handleOnDrop}
+      className={styles.card}
+    >
       <div className={styles.actions}>
         <Heading isBold size="6">
           {title}
         </Heading>
         <div className="menu">Menu</div>
       </div>
-      {cards.map((card) => (
-        <ContentCard key={card.id} card={card} />
+      {cardsLocal.map((card, index) => (
+        <ContentCard
+          onDragEnd={handleCardDragEnd(index, card.id)}
+          onDragStart={handleOnDragStart(card)}
+          draggable
+          key={card.id}
+          card={card}
+        />
       ))}
     </Card>
   );
