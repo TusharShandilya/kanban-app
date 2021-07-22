750e86cb2f267fcdf2cd5ad70581709da24eaefa
TusharShandilya <tusharshandilya@live.com>
2021-07-22 20:35:30 +0530

    Field component jsx added

---
+++ src/components/UI/atoms/Field/Field.jsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames'
+
+import styles from './field.module.scss'
+
+const Field = ({
+  type = 'text',
+  disabled,
+  color = 'basic',
+  size = 'md',
+  className,
+  ...rest
+}) => {
+  const fieldStyles = classNames(
+    styles.field,
+    { [styles.disabled]: disabled },
+    `${styles.color}-${color}`,
+    `${styles.size}-${size}`,
+    className,
+  )
+
+  return <input type={type} className={fieldStyles} {...rest} />
+}
+
+export default Field
