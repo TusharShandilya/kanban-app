7b565a17c1a63a564f521b6e1304ceda26dcb372
TusharShandilya <tusharshandilya@live.com>
2021-07-23 20:06:20 +0530

    Card content is editable on click

--- src/components/UI/organisms/ContentCard/ContentCard.jsx
+++ src/components/UI/organisms/ContentCard/ContentCard.jsx
@@ -1,19 +1,55 @@
-import React from 'react'
-import { faPenSquare } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react'
+import { faPenSquare, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classNames from 'classnames'
 
-import { Card } from '../../atoms'
+import { Card, Field } from '../../atoms'
 
 import styles from './contentCard.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const ContentCard = ({ card }) => {
-  const { content } = card
+const ContentCard = ({ card: { content } }) => {
+  const [isEditable, setIsEditable] = useState(false)
+  const [contentValue, setContentValue] = useState('')
+
+  useEffect(() => {
+    setContentValue(content)
+  }, [content])
+
+  const toggleEditable = () => setIsEditable(!isEditable)
+
+  const handleFieldChange = ({ target: { value } }) => setContentValue(value)
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    /**
+     * TODO:
+     * 1. API call
+     * 2. Redux store hydrate
+     */
+  }
 
   return (
-    <Card className={styles.card}>
-      <div className={styles.content}>{content}</div>
-      <div className={styles.menu}>
-        <FontAwesomeIcon icon={faPenSquare} />
+    <Card
+      className={classNames(styles.card, { [styles.isEditable]: isEditable })}
+    >
+      <div className={styles.content}>
+        {isEditable ? (
+          <form onSubmit={handleFormSubmit}>
+            {/**
+             * TODO: Seperate out this form component
+             */}
+            <Field
+              autoFocus
+              value={contentValue}
+              onChange={handleFieldChange}
+            />
+          </form>
+        ) : (
+          content
+        )}
+      </div>
+      <div className={styles.menu} onClick={toggleEditable}>
+        <FontAwesomeIcon icon={isEditable ? faTimesCircle : faPenSquare} />
       </div>
     </Card>
   )
